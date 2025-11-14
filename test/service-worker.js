1e89bc26f8a85bcd01dc8de046b41a799131ed94
sniperjm <72283428+sniperjm@users.noreply.github.com>
2025-11-14 21:34:48 +0100

    Update service-worker.js

--- test/service-worker.js
+++ test/service-worker.js
@@ -1,2 +1,30 @@
-self.addEventListener("install", e => { self.skipWaiting(); });
-self.addEventListener("fetch", e => {});
+const CACHE_NAME = "owendvh-ckr-setup-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-512.png",
+  "./owendvh-racing-logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
